b8b36c99d06613f6ee14a2dee9a48e03ad3d55a8
Rafael Brito <39818875+raphbrito@users.noreply.github.com>
2026-08-28 16:49:14 -0300

    Add files via upload

--- eventos/odun-ije-rafael-oxum/js/dados-evento.js
+++ eventos/odun-ije-rafael-oxum/js/dados-evento.js
@@ -51,7 +51,7 @@ const EVENTO = {
         brasaoAseEsmeralda: "img/brasao1.png",
         brasaoAseTopazio: "img/brasao2.png",
         favicon: "img/favicon/logo-evento.ico",
-        imagemCompartilhamento: "img/convite-compartilhamento-horizontal.jpg"
+        imagemCompartilhamento: "img/convite-compartilhamento.jpg"
     },
 
     subtituloHero: "Sete anos de caminhada religiosa",
